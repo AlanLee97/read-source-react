5c53d43ea7e1823a74d85e66c9d6e39bf2739bf1
AlanLee <1445654576@qq.com>
2024-09-30 23:53:39 +0800

    fix: adjust demo code

--- demo/react-app-demo/src/App.js
+++ demo/react-app-demo/src/App.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const onClickBtn = () => {
+    setCount(count+1)
+  }
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>count: {count}</h1>
+      <div>
+        <button onClick={onClickBtn}>+1</button>
+      </div>
     </div>
   );
 }
